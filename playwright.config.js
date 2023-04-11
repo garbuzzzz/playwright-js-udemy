b5690a479b51ad26e4cc6e3cf00880be797ed447
garbuzpb <garbuz.pb@gmail.com>
2023-04-11 22:02:58 +0200

    disabled husky

--- playwright.config.js
+++ playwright.config.js
@@ -3,16 +3,16 @@
 const { devices } = require('@playwright/test')
 
 const config = {
-  testDir: './tests',
-  timeout: 30 * 1000,
-  expect: {
-    timeout: 5000,
-  },
-  reporter: 'html',
-  use: {
-    browserName: 'firefox',
-    headless: false,
-  },
+	testDir: './tests',
+	timeout: 30 * 1000,
+	expect: {
+		timeout: 5000,
+	},
+	reporter: 'html',
+	use: {
+		browserName: 'firefox',
+		headless: false,
+	},
 }
 
 module.exports = config
