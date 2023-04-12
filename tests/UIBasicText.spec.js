ae19b5aafc10f691be13992cbfba607cb077a803
garbuzpb <garbuz.pb@gmail.com>
2023-04-12 10:13:25 +0200

    test husky again

--- tests/UIBasicText.spec.js
+++ tests/UIBasicText.spec.js
@@ -9,12 +9,12 @@ test('First Playwright text', async ({ browser, page }) => {
 	console.log(await page.title())
 	await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy')
 	await page.locator('input#username').type('Pavel Garbuz')
-	await page.locator('input#password').type('assadfasdfa8U')
+	page.locator('input#password').type('assadfasdfa8U')
 	await page.locator('input#signInBtn').click()
 	console.log(await page.locator('div[style*=\'none\']').textContent())
-
 	// need timeout more than 5000
 	await expect(page.locator('div[style*=\'none\']')).toContainText(
 		'Incorrect username/password.'
 	)
+	
 })
