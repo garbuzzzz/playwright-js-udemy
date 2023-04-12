8cc1ebaaaf33b5049ce40415b219f63e65d95776
garbuzpb <garbuz.pb@gmail.com>
2023-04-12 20:41:55 +0200

    WAITING CONCEPT!! (that was the end of section3 actually)

--- tests/UIBasicText.spec.js
+++ tests/UIBasicText.spec.js
@@ -16,5 +16,44 @@ test('First Playwright text', async ({ browser, page }) => {
 	await expect(page.locator('div[style*=\'none\']')).toContainText(
 		'Incorrect username/password.'
 	)
+})
 
+// eslint-disable-next-line playwright/no-focused-test
+test.only('filling the fields', async ({ page }) => {
+	await page.goto('https://rahulshettyacademy.com/loginpagePractise')
+	const userName = page.locator('input#username')
+	const password = page.locator('input#password')
+	const signIn = page.locator('input#signInBtn')
+	await userName.fill('rahulshettyacademy')
+	await password.fill('learning')
+	await signIn.click()
+
+	// works fine:
+	// console.log(await page.locator('.card-body a').nth(0)
+	// 	.textContent())
+
+	// it will show empty [] because allTextContents doesn't wait? Meanwhile, textContent WAITS !! :
+	// console.log(await page.locator('.card-body a').allTextContents())
+	// await page.pause()
+
+	//! we can wait explicitly, but it still will be [] because it's not a service oriented app!!!!
+	// await page.waitForLoadState('networkidle')
+	// 	const allContents = await page.locator('.card-body a').allTextContents()
+	// 	console.log(allContents)
+
+	// so, for those apps that don't make api calls to see its content, we can use this approach:
+	await page.waitForURL('https://rahulshettyacademy.com/angularpractice/shop')
+	const allContents = await page.locator('.card-body a').allTextContents()
+	console.log(allContents)
+
+	//! but in the video he used Promise.all, I guess to make sure both click even and waiting are waited simultaneously:
+	// await Promise.all(
+	// 	[
+	// 		signIn.click(),
+	// 		page.waitForURL('https://rahulshettyacademy.com/angularpractice/shop')
+	// 	]
+	// )
+	// const allContents = await page.locator('.card-body a').allTextContents()
+	// console.log(allContents)
+	// so the last approach is the best!
 })
